e7a58322b7563021dec1c1a37360232d3a63634f
austin <auhu8080@corado.edu>
2023-04-29 21:30:27 +0000

    commit 1

--- mm.c
+++ mm.c
@@ -132,79 +132,148 @@ static void *coalesce(void *bp);
 static void printblock(void *bp); 
 static void checkblock(void *bp);
 
-//
-// mm_init - Initialize the memory manager 
-//
-int mm_init(void) 
+
+
+int mm_init(void)
 {
-  //
-  // You need to provide this
-  //
-  return 0;
+/* Create the initial empty heap */
+if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+    return -1;
+PUT(heap_listp, 0); /* Alignment padding */
+PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); /* Prologue header */
+PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); /* Prologue footer */
+PUT(heap_listp + (3*WSIZE), PACK(0, 1)); /* Epilogue header */
+heap_listp += (2*WSIZE);
+
+ /* Extend the empty heap with a free block of CHUNKSIZE bytes */
+ if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+     return -1;
+ return 0;
+ }
+
+//fig 9.45
+static void *extend_heap(uint32_t words)
+{
+char *bp;
+uint32_t size;
+
+/* Allocate an even number of words to maintain alignment */
+size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+if ((long)(bp = mem_sbrk(size)) == -1)
+return NULL;
+
+ /* Initialize free block header/footer and the epilogue header */
+ PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
+ PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
+ PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
+
+ /* Coalesce if the previous block was free */
+ return coalesce(bp);
 }
 
-
-//
-// extend_heap - Extend heap with free block and return its block pointer
-//
-static void *extend_heap(uint32_t words) 
-{
-  //
-  // You need to provide this
-  //
-  return NULL;
-}
-
-
-//
-// Practice problem 9.8
-//
-// find_fit - Find a fit for a block with asize bytes 
-//
-static void *find_fit(uint32_t asize)
-{
-  return NULL; /* no fit */
-}
-
-// 
-// mm_free - Free a block 
-//
+//fig 9.46
 void mm_free(void *bp)
 {
-  //
-  // You need to provide this
-  //
+size_t size = GET_SIZE(HDRP(bp));
+
+PUT(HDRP(bp), PACK(size, 0));
+PUT(FTRP(bp), PACK(size, 0));
+coalesce(bp);
 }
 
-//
-// coalesce - boundary tag coalescing. Return ptr to coalesced block
-//
-static void *coalesce(void *bp) 
+static void *coalesce(void *bp)
 {
-  return bp;
+size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+size_t size = GET_SIZE(HDRP(bp));
+
+if (prev_alloc && next_alloc) { /* Case 1 */
+    return bp;
+}
+else if (prev_alloc && !next_alloc) { /* Case 2 */
+    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size,0));
+}
+else if (!prev_alloc && next_alloc) { /* Case 3 */
+    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+}
+else { /* Case 4 */
+    size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+    GET_SIZE(FTRP(NEXT_BLKP(bp)));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+}
+return bp;
 }
 
-//
-// mm_malloc - Allocate a block with at least size bytes of payload 
-//
-void *mm_malloc(uint32_t size) 
+//fig 9.47
+void *mm_malloc(uint size)
 {
-  //
-  // You need to provide this
-  //
-  return NULL;
-} 
+size_t asize; /* Adjusted block size */
+size_t extendsize; /* Amount to extend heap if no fit */
+char *bp;
 
-//
-//
-// Practice problem 9.9
-//
-// place - Place block of asize bytes at start of free block bp 
-//         and split if remainder would be at least minimum block size
-//
+/* Ignore spurious requests */
+if (size == 0)
+return NULL;
+
+ /* Adjust block size to include overhead and alignment reqs. */
+ if (size <= DSIZE)
+    asize = 2*DSIZE;
+ else
+    asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+ /* Search the free list for a fit */
+ if ((bp = find_fit(asize)) != NULL) {
+    place(bp, asize);
+ return bp;
+ }
+
+ /* No fit found. Get more memory and place the block */
+ extendsize = MAX(asize,CHUNKSIZE);
+ if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+    return NULL;
+ place(bp, asize);
+ return bp;
+ }
+
+ //prob 9.8
+static void *find_fit(uint32_t asize)
+{
+/* First-fit search */
+void *bp;
+
+for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+    if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+        return bp;
+        }
+    }
+ return NULL; /* No fit */
+ 
+ }
+
+// prob 9.9
 static void place(void *bp, uint32_t asize)
 {
-}
+size_t csize = GET_SIZE(HDRP(bp));
+
+if ((csize - asize) >= (2*DSIZE)) {
+    PUT(HDRP(bp), PACK(asize, 1));
+    PUT(FTRP(bp), PACK(asize, 1));
+    bp = NEXT_BLKP(bp);
+    PUT(HDRP(bp), PACK(csize-asize, 0));
+    PUT(FTRP(bp), PACK(csize-asize, 0));
+    }
+ else {
+    PUT(HDRP(bp), PACK(csize, 1));
+    PUT(FTRP(bp), PACK(csize, 1));
+    }
+ }
 
 
 //
